d4dab9adb19eb0f9714773564c09c0fef8f6e760
moxgit <moxmosi@gmail.com>
2016-12-30 01:55:40 +0100

    nichts

--- mobile/src/main/java/mi/hci/luh/de/smartwatchcommunication/SensorAnalysis.java
+++ mobile/src/main/java/mi/hci/luh/de/smartwatchcommunication/SensorAnalysis.java
@@ -7,7 +7,6 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -400,23 +399,20 @@ public class SensorAnalysis extends FragmentActivity implements GoogleApiClient.
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        Log.d("onConnected", "");
+
     }
 
     @Override
     public void onPause() {
         super.onPause();
         timerHandler.removeCallbacks(timerRunnable);
-        Log.d("onPause", "");
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        Log.d("onConnectionSuspended", "");
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d("onConnectionFailed", "");
     }
 }
